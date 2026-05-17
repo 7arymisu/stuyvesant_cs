40302f53e1ecaa5dbf34a50290ee0d815bb2af0e
Taryn Huang <9huangt@gmail.com>
2026-05-17 07:45:34 -0400

    feat: added pokemon dictionary

--- projects/pokedex/makePokedex.py
+++ projects/pokedex/makePokedex.py
@@ -2,9 +2,9 @@
 print("Content-Type: text/html\n\n")
 
 # used when debugging on the web
-import os # import for chmod
+'''import os # import for chmod
 import cgitb # import to catch HTTP errors (when running on the web)
-cgitb.enable() # enable your error output for HTTP
+cgitb.enable() # enable your error output for HTTP'''
 
 # constant
 INDENT = "  "
@@ -30,3 +30,18 @@ page = '''
   </body>
 </html>
 '''
+
+with open('pokemon.csv', "r") as f:
+    pokedata =f.read().strip().split("\n")
+    
+    stats = pokedata[0].split(",")
+    pokedict = {}
+
+    for entry in pokedata[1:]:
+        pokeinfo = entry.split(",")
+        key = pokeinfo[0]
+
+        pokedict[key] = {}
+
+        for i, data in enumerate(pokeinfo[1:]):
+            pokedict[key][stats[i+1]] = data
