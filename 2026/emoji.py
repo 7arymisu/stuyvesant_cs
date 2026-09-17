41f65392ed20b436b0aff2da60eee51b8825121b
Pâtissier <tarynh5@nycstudents.net>
2026-09-17 15:10:47 +0000

    Create emoji.py

---
+++ 2026/emoji.py
@@ -0,0 +1,15 @@
+def setup():
+    size(500,500)
+
+def draw():
+    background(220)
+    fill(255,255,0)
+    stroke(0)
+    stroke_weight(5)
+    circle(250,250,400)
+    fill(255,255,0)
+    stroke_weight(1)
+    circle(150,250,100)
+    fill(255,255,0)
+    stroke_weight(6)
+    circle(350,250,100)
